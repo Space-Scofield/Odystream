7b7a311a32e2a97fadf7a5711f85a7f5053f654a
Space <InformaticSpace2580@gmail.com>
2025-12-30 14:30:17 +0100

    Implement logging for video file management
    Added logging functionality to track file movements and directory cleanup and corrected some issues.

--- main.py
+++ main.py
@@ -1,48 +1,57 @@
 from pathlib import Path
 import re
 import shutil
+import logging
 
 MOVIES = Path("./srv/media/movies")
 SERIES = Path("./srv/media/series")
 DOWNLOADS = Path("./srv/downloads")
 
-VIDEO_EXTENSIONS = ["*.mkv","*.mp4", "*.avi"]
+VIDEO_EXTENSIONS = {".mkv",".mp4", ".avi"}
 SERIES_PATTERN = re.compile(r"\b\d+x\d{2}\b", re.IGNORECASE)
 
-def main():
-    scan_directory(DOWNLOADS)
-    cleanup_empty_dirs(DOWNLOADS)
-
-if __name__ == "__main__":
-    main()
-
-def scan_directory(root: Path):
-    for item in root.rglob("*"):
-        if is_video(item):
-            move_video(item)
-
-def classify(file: Path) -> Path:
-    return SERIES if is_series(file) else MOVIES
-
-def is_video(file: Path):
-    file.is_file() and file.suffix.lower() in VIDEO_EXTENSIONS
+def is_video(file: Path) -> bool:
+    return file.is_file() and file.suffix.lower() in VIDEO_EXTENSIONS
 
 def is_series(file: Path) -> bool:
     return bool(SERIES_PATTERN.search(file.name))
  
+def classify(file: Path) -> Path:
+    return SERIES if is_series(file) else MOVIES
+
 def move_video(file: Path):
     destination = classify(file) / file.name
     shutil.move(str(file), destination)
+    if classify is SERIES: logging.info(f"The file {file.name} is a series")
+    else: logging.info(f"The file {file.name} is a movie")
+
+def scan_directory(root: Path):
+    for item in root.rglob("*"):
+        if is_video(item):
+            logging.info(f"The file {item} is a multi-media file, it will be moved")
+            move_video(item)
 
 def cleanup_empty_dirs(root: Path):
     for dirpath in sorted(root.rglob("*"), reverse=True):
-        if dirpath.is_dir and not any(dirpath.iterdir()):
+        logging.info(f"The founded directories are:{dirpath}")
+        if dirpath.is_dir() and not any(dirpath.iterdir()):
             dirpath.rmdir()
+            logging.info(f"No files found, deleting directories...")
 
 
+    
 def main():
+# Logging
+    logging.basicConfig(
+        filename='logs.txt',
+        level=logging.DEBUG,
+        format='%(asctime)s | %(levelname)s | %(message)s'
+    )
+# Program
     scan_directory(DOWNLOADS)
+    logging.debug(f"The scan has started in {DOWNLOADS}")
     cleanup_empty_dirs(DOWNLOADS)
+    logging.debug(f"The cleanup process has started in {DOWNLOADS}")
 
 if __name__ == "__main__":
     main()
